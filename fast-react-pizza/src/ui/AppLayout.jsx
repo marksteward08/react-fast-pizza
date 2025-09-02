655a5c0e5a230d96da87e3c24d45fe37b5b26191
Mark Steward Bayot <marksteward08@gmail.com>
2025-09-02 20:55:43 +0800

    Add Loading component and integrate loading state in AppLayout

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loading from "../Loading";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loading />}
+
       <Header />
       <main>
         <Outlet />
